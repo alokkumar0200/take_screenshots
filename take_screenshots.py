35a99beca1eacf3a1d8c10d0895fb885fac36ac2
Alok kumar <alokkumar0200@gmail.com>
2019-12-24 14:47:56 +0530

    added headless mode

--- take_screenshots.py
+++ take_screenshots.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 import sys
 import os, time
 
@@ -11,7 +12,8 @@ html = ''
 domain = ''
 output = ''
 folder_for_results = '/home/devil/Documents/results/'				#Change this path to results directory
-selenium_driver = '/home/devil/geckodriver/geckodriver'				#Change this to path of selenium driver (geckodriver for firefox | chromedriver for chrome)
+selenium_driver = '/home/devil/take_screenshots/geckodriver'				#Change this to path of selenium driver (geckodriver for firefox | chromedriver for chrome)
+TEMPLATE_FILE = '/home/devil/take_screenshots/template'
 
 def check_args():
 	global dirs, folder_for_results
@@ -49,7 +51,7 @@ def gen_report(path, domain):
 	global report
 	text = ''
 
-	with open('./template') as file:
+	with open(TEMPLATE_FILE) as file:
 		text = file.read()
 		text = text.replace('PATH', path)
 		text = text.replace('DOMAIN', domain)
@@ -62,11 +64,14 @@ def main():
 	args= check_args()
 	try:
 		if os.path.exists(selenium_driver):
-			browser = webdriver.Firefox(executable_path=selenium_driver)			#Comment this line if using chrome instead of firefox
-			# browser = webdriver.Chrome(executable_path=selenium_driver)			#And uncomment this line for Chrome
+			options = Options()
+			options.add_argument('--headless')
+			browser = webdriver.Firefox(options=options, executable_path=selenium_driver)			#Comment this line if using chrome instead of firefox
+			# browser = webdriver.Chrome(options=options, executable_path=selenium_driver)			#And uncomment this line for Chrome
 			for x in dirs:
 				image = x.split(' ')[0].strip('/')[:-1]+'.png'
 				addr = domain+'/'+x.split(' ')[0].strip('/')
+				print('[*] Domain: %s' %(addr))
 				browser.get(addr)
 				browser.save_screenshot(folder_for_results + '/'+image)
 				gen_report(image, addr)
